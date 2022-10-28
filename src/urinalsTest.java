bbb7d2491d94869d16d00704203a757693e4961b
Saurav Sarkar <sauravs@asu.edu>
2022-10-27 19:55:33 -0700

    Added testcases to get count of urinals.

--- src/urinalsTest.java
+++ src/urinalsTest.java
@@ -11,15 +11,50 @@ public class urinalsTest {
         urine = new urinals();
     }
 
+//    @Test
+//    void goodStringValid() {
+//        assertEquals(true, urine.goodString("====== Saurav Sarkar == TEST FIRST EXECUTED ======="));
+//    }
+//
+//    @Test
+//    void goodStringInValid() {
+//        assertEquals(false, urine.goodString("====== Saurav Sarkar == TEST TWO EXECUTED ======="));
+//    }
+
     @Test
-    void goodStringValid() {
-        assertEquals(true, urine.goodString("====== Saurav Sarkar == TEST FIRST EXECUTED ======="));
+    void getFreeUrinalsOne() {
+        System.out.println("====== Saurav Sarkar == TEST ONE EXECUTED =======");
+        assertEquals(1, urine.getFreeUrinals("10001"));
     }
 
     @Test
-    void goodStringInValid() {
-        assertEquals(false, urine.goodString("====== Saurav Sarkar == TEST TWO EXECUTED ======="));
+    void getFreeUrinalsTwo() {
+        System.out.println("====== Saurav Sarkar == TEST TWO EXECUTED =======");
+        assertEquals(0, urine.getFreeUrinals("1001"));
     }
 
+    @Test
+    void getFreeUrinalsThree() {
+        System.out.println("====== Saurav Sarkar == TEST THREE EXECUTED =======");
+        assertEquals(3, urine.getFreeUrinals("00000"));
+    }
+
+    @Test
+    void getFreeUrinalsFour() {
+        System.out.println("====== Saurav Sarkar == TEST FOUR EXECUTED =======");
+        assertEquals(2, urine.getFreeUrinals("0000"));
+    }
+
+    @Test
+    void getFreeUrinalsFive() {
+        System.out.println("====== Saurav Sarkar == TEST FIVE EXECUTED =======");
+        assertEquals(1, urine.getFreeUrinals("01000"));
+    }
+
+    @Test
+    void getFreeUrinalsSix() {
+        System.out.println("====== Saurav Sarkar == TEST SIX EXECUTED =======");
+        assertEquals(-1, urine.getFreeUrinals("011"));
+    }
 
 }
